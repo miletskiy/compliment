04b7bb36357ed7b4d1031a4a7c1fa480bfe699c6
miletskiy <s.miletskiy@gmail.com>
2017-03-04 20:00:25 +0200

    Add Uploadphoto form

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,17 @@
 from django import forms
+from .models import Photo
+
 
 class NameForm(forms.Form):
     user_name = forms.CharField(label='User name', max_length=100)
+
+
+class UploadPhotoForm(forms.ModelForm):
+    """
+    Form for uploading photo
+    """
+
+    class Meta:
+        model = Photo
+        fields = "__all__"
+
--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
-from .forms import NameForm
+from .forms import (
+    NameForm,
+    UploadPhotoForm,
+)
+
 
 def index(request):
     """
@@ -46,4 +51,21 @@ def photo(request):
     Photo view
     """
 
-    return render(request, 'photo.html')
+
+    if request.method == "POST":
+        form = UploadPhotoForm(data=request.POST, files=request.FILES, )
+        if form.is_valid():
+
+            fcd = form.cleaned_data
+            photo = fcd.get("preview")
+            print photo
+
+            form.save()
+
+            messages.success(request, u"Success")
+        else:
+            form = UploadPhotoForm()
+
+    form = UploadPhotoForm()
+
+    return render(request, 'photo.html', {"form": form })
